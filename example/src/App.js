1c2e9f95628f175f813b3ffd82f6e0e54c411157
ivantacca <taccadoliivan@gmail.com>
2022-07-09 17:52:17 +0200

    Added date indicator

--- example/src/App.js
+++ example/src/App.js
@@ -9,28 +9,31 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={()=>setMonth(month-1)}><ChevronLeft/></button>
-      <Calendar 
-        month={{ year: 2022, month }}
-        value={date}
-        onChange={setDate}
-        closedDays={['2022-06-15']}
-        closedPastDays={false}
-        indicator={'show-year'}
-        onIndicatorClick={()=>alert('react-minimal-calendar is 🔥')}
-        multiselect={false}
-        header={['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']}
-        layout="fixed"
-        palette={{
-          primary: "darkblue",
-          selection: "pink",
-          accent: "white"
-        }}
-        daySize={40}
-        fontSize={14}
-        fontFamily={'Poppins'}
-      />
-      <button onClick={()=>setMonth(month+1)}><ChevronRight/></button>
+      <div className="row">
+        <button onClick={()=>setMonth(month-1)}><ChevronLeft/></button>
+        <Calendar 
+          month={{ year: 2022, month }}
+          value={date}
+          onChange={setDate}
+          closedDays={['2022-06-15']}
+          closedPastDays={false}
+          indicator={'show-year'}
+          onIndicatorClick={()=>alert('react-minimal-calendar is 🔥')}
+          multiselect={false}
+          header={['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']}
+          layout="fixed"
+          palette={{
+            primary: "darkblue",
+            selection: "pink",
+            accent: "white"
+          }}
+          daySize={40}
+          fontSize={16}
+          fontFamily={'Poppins'}
+        />
+        <button onClick={()=>setMonth(month+1)}><ChevronRight/></button>
+      </div>
+      {date ? <span className="abs-date-indicator">{date}</span> : null}
     </div>
   );
 }
