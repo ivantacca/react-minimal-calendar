a1dd6b10c049d6df2520b3e446b72031e8fb5d0f
ivantacca <taccadoliivan@gmail.com>
2022-07-14 10:25:04 +0200

    Added random palette export

--- example/src/const.js
+++ example/src/const.js
@@ -1,12 +1,40 @@
-export const palette = {
-  light: {
-    primary: "darkblue",
+const lightPalettes = [
+  {
+    primary: "#3838d1",
     selection: "pink",
     accent: "white"
   },
-  dark: {
-    primary: "orangered",
-    selection: "palegreen",
-    accent: "black"
+  {
+    primary: "#be3326",
+    selection: "#efefef",
+    accent: "#be3326"
+  },
+  {
+    primary: "#8971d9",
+    selection: "#e3dbfd",
+    accent: "white"
   }
+]
+
+const darkPalettes = [
+  {
+    primary: "palegreen",
+    selection: "#df3232",
+    accent: "white"
+  },
+  {
+    primary: "#c9ff5c",
+    selection: "#3e32a1",
+    accent: "white"
+  },
+  {
+    primary: "#b8e0d5",
+    selection: "#699186",
+    accent: "white"
+  },
+]
+
+export const palette = {
+  light: lightPalettes[Math.floor(Math.random()*lightPalettes.length)],
+  dark: darkPalettes[Math.floor(Math.random()*darkPalettes.length)]
 }
